d41bd618472c05f7633ddf09678624eb2a51bcf0
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2024-09-12 19:57:45 +0200

    Update index.jsx

--- src/components/imageCollage/index.jsx
+++ src/components/imageCollage/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './imageCollage.module.scss';
-import Image  from 'react-bootstrap/Image';
+//import Image  from 'react-bootstrap/Image';
 
 export default function ImageCollage() {
 
