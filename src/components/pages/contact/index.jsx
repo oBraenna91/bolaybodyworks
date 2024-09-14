fc2fc32f1d1343ed135c3b56344133e5ccceccc3
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2024-09-14 19:55:24 +0200

    contact

--- src/components/pages/contact/index.jsx
+++ src/components/pages/contact/index.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 export default function ContactPage() {
     return(
-        <div>
-            CONTACT US
+        <div className="appearing-container">
+            <h1 className="text-center">Kontakta oss!</h1>
+            <div className="m-auto text-center">
+                <div>Gymfluence OÜ, 1686497</div>
+                <div>jk@gymfluence.io</div>
+                <div>Paepargi 43, 28, 11417 Tallinn, Estonia</div>
+                <div>+37255574987</div>
+            </div>
+            
         </div>
     )
 }
